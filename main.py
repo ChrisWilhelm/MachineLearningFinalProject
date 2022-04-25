01bbc01c873ee563ff2126528deb458822f9a880
jaydenkunwar <kunwarjayden@gmail.com>
2022-04-25 14:49:32 -0400

    dataset uploaded

--- main.py
+++ main.py
@@ -4,12 +4,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 def read_data():
-    sample_data = pd.read_excel('Kunwar_CancerSEEK_Data.xlsx')
-    sample_pd = sample_data.iloc[:, 1:]
-    sample_np = (sample_data.iloc[:, 1:]).to_numpy()
-    cancer_predictions = sample_np[:, 9]
-    cancer_data = sample_np[:, :9]
-    return cancer_data, cancer_predictions
+    sample_data = pd.read_csv('dataset/Consolidated_CancerSEEK_Data.csv')
+    print(sample_data)
+
 
 def visualize_data(X, y):
     df = pd.DataFrame(data=X, columns=['Omega', 'CA-125', 'CA19-9', 'CEA', 'HGF', 'Myleoperoxidase',
@@ -21,5 +18,4 @@ def visualize_data(X, y):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    read_data()

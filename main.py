80b65c09a02def44eceacf0969fe02dfea5f6c0a
jaydenkunwar <kunwarjayden@gmail.com>
2022-04-25 14:40:52 -0400

    creating main

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def read_data():
+    sample_data = pd.read_excel('Kunwar_CancerSEEK_Data.xlsx')
+    sample_pd = sample_data.iloc[:, 1:]
+    sample_np = (sample_data.iloc[:, 1:]).to_numpy()
+    cancer_predictions = sample_np[:, 9]
+    cancer_data = sample_np[:, :9]
+    return cancer_data, cancer_predictions
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+def visualize_data(X, y):
+    df = pd.DataFrame(data=X, columns=['Omega', 'CA-125', 'CA19-9', 'CEA', 'HGF', 'Myleoperoxidase',
+                                       'OPN', 'Prolactin', 'TIMP 1'])
+    sns.heatmap(df.corr(),
+                xticklabels=df.corr().columns.values,
+                yticklabels=df.corr().columns.values)
+    plt.show()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

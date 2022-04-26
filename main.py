dcf918f229a82d7460193154fce69051aa81f69d
jaydenkunwar <kunwarjayden@gmail.com>
2022-04-26 15:33:46 -0400

    proper data uploaded, NaN values set to 0 (may want to avg it), dataframes created

--- main.py
+++ main.py
@@ -5,17 +5,23 @@ import pandas as pd
 
 def read_data():
     sample_data = pd.read_csv('dataset/Consolidated_CancerSEEK_Data.csv')
-    print(sample_data)
+    sample_data.fillna(0)
+    pd.set_option("display.max_rows", None, "display.max_columns", None)
+    demographic_biomarker_data = sample_data.drop(["CancerSEEK Logistic Regression Score", "CancerSEEK Test Result"], axis=1)
+    biomarker_data = demographic_biomarker_data.drop(["Age", "Sex"], axis=1)
+    replicated_biomarker_data = biomarker_data[["Ω score", "CA-125 (U/ml)", "CA19-9 (U/ml)", "CEA (pg/ml)", "HGF (pg/ml)",
+                                                "Myeloperoxidase (ng/ml)", "OPN (pg/ml)", "Prolactin (pg/ml)", "TIMP-1 (pg/ml)",
+                                                "Ground Truth"]]
+    return demographic_biomarker_data, biomarker_data, replicated_biomarker_data
 
 
-def visualize_data(X, y):
-    df = pd.DataFrame(data=X, columns=['Omega', 'CA-125', 'CA19-9', 'CEA', 'HGF', 'Myleoperoxidase',
-                                       'OPN', 'Prolactin', 'TIMP 1'])
+def visualize_data(df):
     sns.heatmap(df.corr(),
                 xticklabels=df.corr().columns.values,
                 yticklabels=df.corr().columns.values)
     plt.show()
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    read_data()
+    demographic_biomarker_data, biomarker_data, replicated_biomarker_data = read_data()

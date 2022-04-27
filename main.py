1168d2484041309fc799418af9850727dbd0d40d
jaydenkunwar <kunwarjayden@gmail.com>
2022-04-26 21:08:16 -0400

    updated dataset processing

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import pandas as pd
 
 def read_data():
     sample_data = pd.read_csv('dataset/Consolidated_CancerSEEK_Data.csv')
-    sample_data.fillna(0)
-    pd.set_option("display.max_rows", None, "display.max_columns", None)
+    sample_data = sample_data.drop(["Patient ID #", "Sample ID #", "Tumor type", "Race", "AJCC Stage"], axis=1)
     demographic_biomarker_data = sample_data.drop(["CancerSEEK Logistic Regression Score", "CancerSEEK Test Result"], axis=1)
     biomarker_data = demographic_biomarker_data.drop(["Age", "Sex"], axis=1)
-    replicated_biomarker_data = biomarker_data[["Ω score", "CA-125 (U/ml)", "CA19-9 (U/ml)", "CEA (pg/ml)", "HGF (pg/ml)",
-                                                "Myeloperoxidase (ng/ml)", "OPN (pg/ml)", "Prolactin (pg/ml)", "TIMP-1 (pg/ml)",
-                                                "Ground Truth"]]
+    replicated_biomarker_data = biomarker_data[
+        ["Ω score", "CA-125 (U/ml)", "CA19-9 (U/ml)", "CEA (pg/ml)", "HGF (pg/ml)",
+         "Myeloperoxidase (ng/ml)", "OPN (pg/ml)", "Prolactin (pg/ml)", "TIMP-1 (pg/ml)",
+         "Ground Truth"]]
     return demographic_biomarker_data, biomarker_data, replicated_biomarker_data
 
 
@@ -25,5 +25,3 @@ def visualize_data(df):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     demographic_biomarker_data, biomarker_data, replicated_biomarker_data = read_data()
-    print(demographic_biomarker_data.describe())
-    print(biomarker_data.describe())

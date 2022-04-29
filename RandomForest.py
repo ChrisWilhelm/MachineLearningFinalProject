c30b5c994e6fa2ec2f6389d2427136156cc951bf
ChrisWilhelm <chriswilhelm2019@gmail.com>
2022-04-29 10:16:15 -0400

    Work on random forests to get training accuracy and to exclude test data from the training set

--- RandomForest.py
+++ RandomForest.py
@@ -31,13 +31,25 @@ class RandomForest():
 def modeling(data):
     np_dataset = data.to_numpy()
     num_col = np.shape(np_dataset)[1]
-    X = np_dataset[:, :num_col - 1]
-    y = np_dataset[:, num_col - 1]
+    num_rows = np.shape(np_dataset)[0] - 100
+    X = np_dataset[:num_rows, :num_col - 1]
+    y = np_dataset[:num_rows, num_col - 1]
     num_folds = KFold(n_splits=10, shuffle=True, random_state=553)
+    best_model = -1
+    best_model_acc = 0
     results = list()
     for train_x, test_x in num_folds.split(X):
+        print('Next Set:')
         X_train, X_test = X[train_x, :], X[test_x, :]
         Y_train, Y_test = y[train_x], y[test_x]
+        numElements = X_train.shape[0]
+        numLastIter = numElements % 50
+        model = RandomForest(num_estimators=15, max_depth=5, min_samples_split=2, criterion='entropy')
+        for i in range(1, (numElements - numLastIter)//100):
+            model.fit(X_train[:i*100], Y_train[:i*100])
+            print('Iteration: ' + str(i) + ' is ' + str(accuracy_score(Y_train[:i*100], model.predict(X_train[:i*100]))))
+        model.fit(X_train, Y_train)
+        print('Total Training Accuracy: ' + str(accuracy_score(Y_train, model.predict(X_train))))
         # best params found n_estimators = 20, max_depth = 15 min_samples_split = 3, criterion='entropy'
         # variables = dict()
         # variables['n_estimators'] = [5, 10, 15, 20]
@@ -52,24 +64,54 @@ def modeling(data):
         # acc = accuracy_score(Y_test, Y_pred)
         # results.append(acc)
         # print('>acc=%.3f, est=%.3f, cfg=%s' % (acc, best_hyper.best_score_, best_hyper.best_params_))
-        model = RandomForest(num_estimators=20, max_depth=15, min_samples_split=3, criterion='entropy')
         model.fit(X_train, Y_train)
         Y_pred = model.predict(X_test)
         acc = accuracy_score(Y_test, Y_pred)
+        print('Test Accuracy for the set: ' + str(acc))
+        if acc > best_model_acc:
+            best_model = model
+            best_model_acc = acc
         results.append(acc)
-    return results
+    return best_model
 
 
 def main():
     dem_bio_data, biomarker_data, replicated_biomarker_data = read_data()
-    print(np.mean(modeling(dem_bio_data)))
-    print(np.mean(modeling(biomarker_data)))
-    print(np.mean(modeling(replicated_biomarker_data)))
+    dem_bio = dem_bio_data.to_numpy()
+    num_col = np.shape(dem_bio)[1]
+    X_dem_bio = dem_bio[:, :num_col - 1]
+    y_dem_bio = dem_bio[:, num_col - 1]
+    X_dem_bio_test = X_dem_bio[:-100]
+    y_dem_bio_test = y_dem_bio[:-100]
+    bio = biomarker_data.to_numpy()
+    num_col = np.shape(bio)[1]
+    X_bio = bio[:, :num_col - 1]
+    y_bio = bio[:, num_col - 1]
+    X_bio_test = X_bio[:-100]
+    y_bio_test = y_bio[:-100]
+    rep = replicated_biomarker_data.to_numpy()
+    num_col = np.shape(rep)[1]
+    X_rep = rep[:, :num_col - 1]
+    y_rep = rep[:, num_col - 1]
+    X_rep_test = X_rep[:-100]
+    y_rep_test = y_rep[:-100]
+    dem_model = modeling(dem_bio_data)
+    bio_model = modeling(biomarker_data)
+    replicated_model = modeling(replicated_biomarker_data)
+    Y_pred = dem_model.predict(X_dem_bio_test)
+    print(accuracy_score(y_dem_bio_test, Y_pred))
+    Y_pred = bio_model.predict(X_bio_test)
+    print(accuracy_score(y_bio_test, Y_pred))
+    Y_pred = replicated_model.predict(X_rep_test)
+    print(accuracy_score(y_rep_test, Y_pred))
+    # print(dem_res)
+    # print(bio_res)
+    # print(replicated_res)
     """
     test accuracy for the 3 data types
-    0.9653208669783255
-    0.9543075708821565
-    0.9042104304535243
+    0.9638905066977286
+    0.9621432731508445
+    0.9196272568433314
     """
 
 
